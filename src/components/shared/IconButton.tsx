df070030f2bfda94b8c0cfda9e4106a96926e462
Archit Garg <thearchitgargg@gmail.com>
2025-01-19 15:05:29 +0530

    refactor: make icon button use native button

--- src/components/shared/IconButton.tsx
+++ src/components/shared/IconButton.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Button from './Button.tsx'
 
 interface Props {
   icon: React.ElementType
@@ -7,9 +6,9 @@ interface Props {
 
 const IconButton: React.FC<Props> = ({ icon: Icon }) => {
   return (
-    <Button variant='outlined' className='!p-2 border-blue-500'>
+    <button className='rounded-lg focus:outline-none p-2 border border-blue-500 bg-white hover:bg-blue-50 transition-all'>
       <Icon className='h-6 w-6 text-blue-500' />
-    </Button>
+    </button>
   )
 }
 
