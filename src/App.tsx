19ccf60cb5e1d217900dd2add64ebc738b6a4b04
Archit Garg <thearchitgargg@gmail.com>
2025-01-19 13:41:16 +0530

    feat: add app logo comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { CubeIcon } from '@heroicons/react/20/solid'
 import './App.css'
 import Card from './components/shared/Card.tsx'
 import H1 from './components/shared/H1.tsx'
+import AppLogo from './components/shared/AppLogo.tsx'
 
 function App() {
   return (
     <div className='mx-auto max-w-7xl sm:px-6 lg:px-8 mt-20'>
       <Card className='px-4 py-5 sm:p-6'>
         <div className='flex items-center'>
-          <CubeIcon className='h-16 w-16 inline-block' />
+          <AppLogo />
           <H1>Dan's Memory lane</H1>
         </div>
       </Card>
---
+++ src/components/shared/AppLogo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CubeIcon } from '@heroicons/react/20/solid'
+
+interface Props {}
+
+const AppLogo: React.FC<Props> = () => {
+  return <CubeIcon className='h-16 w-16 inline-block' />
+}
+
+export default AppLogo
