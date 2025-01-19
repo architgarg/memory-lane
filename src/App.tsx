bd7b1f9c17206721315dafbb2ba5fee6ec2b4f94
Archit Garg <thearchitgargg@gmail.com>
2025-01-19 13:39:44 +0530

    feat: add reusable h1 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { CubeIcon } from '@heroicons/react/20/solid'
 import './App.css'
 import Card from './components/shared/Card.tsx'
+import H1 from './components/shared/H1.tsx'
 
 function App() {
   return (
-    <div>
-      <div className='mx-auto max-w-7xl sm:px-6 lg:px-8 mt-32'>
-        <Card className='px-4 py-5 sm:p-6'>
-          <div className='flex items-center'>
-            <CubeIcon className='h-16 w-16 inline-block' />
-            <h1 className='text-4xl font-semibold text-gray-900 mb-4 ml-4 mt-4'>
-              Dan's Memory lane
-            </h1>
-          </div>
-        </Card>
-      </div>
+    <div className='mx-auto max-w-7xl sm:px-6 lg:px-8 mt-20'>
+      <Card className='px-4 py-5 sm:p-6'>
+        <div className='flex items-center'>
+          <CubeIcon className='h-16 w-16 inline-block' />
+          <H1>Dan's Memory lane</H1>
+        </div>
+      </Card>
     </div>
   )
 }
