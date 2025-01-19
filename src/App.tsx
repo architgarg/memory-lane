92b89ab107ac5523cd3f409b383fc89acc12cf20
Archit Garg <thearchitgargg@gmail.com>
2025-01-19 13:43:51 +0530

    feat: add reusable container comp

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import './App.css'
 import Card from './components/shared/Card.tsx'
 import H1 from './components/shared/H1.tsx'
 import AppLogo from './components/shared/AppLogo.tsx'
+import Container from './components/shared/Container.tsx'
 
 function App() {
   return (
-    <div className='mx-auto max-w-7xl sm:px-6 lg:px-8 mt-20'>
+    <Container className='mt-20'>
       <Card className='px-4 py-5 sm:p-6'>
         <div className='flex items-center'>
           <AppLogo />
           <H1>Dan's Memory lane</H1>
         </div>
       </Card>
-    </div>
+    </Container>
   )
 }
 
