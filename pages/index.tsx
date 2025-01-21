31b9b06e48aa76f0567e6332a6c14a4269058bb1
Archit Garg <thearchitgargg@gmail.com>
2025-01-22 00:00:01 +0530

    feat: update message

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Index = () => {
   return (
     <Container className="pt-10">
       <div className='bg-white p-10 rounded-xl max-w-lg m-auto'>
-        <h1 className='text-3xl font-bold mb-4'>Create a Memory Lane</h1>
+        <h1 className='text-xl font-bold mb-4'>Start by creating a Memory Lane</h1>
         <CreateMemoryLaneForm />
       </div>
     </Container>
