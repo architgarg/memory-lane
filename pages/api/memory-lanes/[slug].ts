fce52ae6e6e1f5118bf465aa1269732343c1242e
Archit Garg <thearchitgargg@gmail.com>
2025-01-21 16:44:38 +0530

    feat: add api to fetch memory lane by slug

--- pages/api/memory-lanes/[slug].ts
+++ pages/api/memory-lanes/[slug].ts
@@ -23,8 +23,23 @@ export default async function handler(
         error: error instanceof Error ? error.message : 'An error occurred',
       })
     }
+  } else if (req.method === 'GET') {
+    try {
+      const memoryLane = await prisma.memoryLane.findUnique({
+        where: { slug: slug as string },
+      })
+      if (memoryLane) {
+        res.json({ memoryLane })
+      } else {
+        res.status(404).json({ error: 'Memory lane not found' })
+      }
+    } catch (error: unknown) {
+      res.status(400).json({
+        error: error instanceof Error ? error.message : 'An error occurred',
+      })
+    }
   } else {
-    res.setHeader('Allow', ['PUT'])
+    res.setHeader('Allow', ['PUT', 'GET'])
     res.status(405).end(`Method ${req.method} Not Allowed`)
   }
 }
--- src/services/memory-lane.service.ts
+++ src/services/memory-lane.service.ts
@@ -1,20 +1,21 @@
 import ApiClient from './api-client.ts'
 import { MEMORY_LANES } from '../constants/api-urls.ts'
+import { MemoryLane } from '@prisma/client'
 
 class MemoryLanesService extends ApiClient {
   async create(user_name: string, slug: string, description: string) {
     return this.post(MEMORY_LANES, { user_name, slug, description })
   }
 
-  async update(
-    slug: string,
-    user_name: string,
-    description: string,
-  ) {
+  async update(slug: string, user_name: string, description: string) {
     return this.put(`${MEMORY_LANES}/${slug}`, {
       user_name,
       description,
     })
+  }
+
+  async getBySlug(slug: string) {
+    return this.get<MemoryLane>(`${MEMORY_LANES}/${slug}`)
   }
 }
 
